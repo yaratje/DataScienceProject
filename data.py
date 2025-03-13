e84767a5078126c4c98f371816e54c28b27e2d25
yaratje <yaratje48@gmail.com>
2025-03-13 09:46:45 +0100

    add setup, fix split_data

--- data.py
+++ data.py
@@ -3,6 +3,28 @@ All stuff related to data
 """
 
 import torch
+from sklearn.model_selection import train_test_split
+import numpy as np
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+
+""""
+Setup basic variables, get dataset from local folder, create dataloader, and extract labels and images for later use
+"""
+def setup():
+    #TRANSFORM from assignment 2
+    transform = transforms.Compose([transforms.Resize(32), transforms.CenterCrop(32), transforms.ToTensor(), transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5])])
+
+    global dataset
+    dataset = ImageFolder(root="./Incidents-subset", transform=transform)
+    global dataloader
+    dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+    global labels 
+    labels = np.array(dataset.targets)
+    global images
+    images = np.array([img for img, label in dataset])
+
 
 
 """"
@@ -10,14 +32,10 @@ Function that splits dataset into train and test datasets
 @dataset, the entire dataset
 @ratio, ratio beteen 0-1 what the split should be
 """
-def train_test_split(dataset, ratio):
-    
-    # Ex. 2.1a your code here
-    train_length = int(len(dataset) * ratio)
-    test_length = len(dataset) - train_length
+def split_data(dataset, ratio):
+    #stratify=labels = balanced labels distribution :O
+    train_data_x,test_data_x,train_data_y,test_data_y = train_test_split(images,labels, test_size=ratio, stratify=labels)
 
-    labels = [dataset.targets[i] for i in range(len(dataset))]
-    img =  [img for img, data in dataset]
-    training_data_x, testing_data_x = torch.utils.data.random_split(img, [train_length, test_length])
-    training_data_y, testing_data_y = torch.utils.data.random_split(labels, [train_length, test_length])
-    return training_data_x, testing_data_x, training_data_y, testing_data_y
+    return train_data_x,test_data_x,train_data_y,test_data_y
+    
+train_data_x, test_data_x , train_data_y, test_data_y = split_data(dataset, 0.2)
